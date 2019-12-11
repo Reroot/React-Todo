d94afe97836d44ae77586a6c791ece27980aa0f1
Artem <triple8six@gmail.com>
2019-12-11 04:21:49 -0800

    Finished

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 export default function Header() {
 	const headerStyle = {
 		backround: "#693090",
@@ -8,6 +8,8 @@ export default function Header() {
 	return (
 		<div style={headerStyle}>
 			<h2>Make your little Dreams come true, one tick at a time</h2>
+			{/* //this is router syntax a tag aren't used instead use LINK */}
+			<Link to="/"> Home </Link> | <Link to="/about"> About </Link>
 		</div>
 	);
 }
